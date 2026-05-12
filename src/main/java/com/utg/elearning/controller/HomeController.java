1b1c1c9cd190315edcbf9fbc2414cc2aac5cd59b
yunusabah <bahy1878@gmail.com>
2026-05-12 15:07:18 +0000

    added HomeController.java

---
+++ src/main/java/com/utg/elearning/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.utg.elearning.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "E-Learning System Running Successfully";
+    }
+}
